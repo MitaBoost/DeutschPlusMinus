efd4225d502689abbd28561a6bd924b3d19ed10b
Mita Boost <mitaboost@gmail.com>
2025-04-17 10:16:31 +0000

    Make the color of the topics black

--- src/components/topic-selector.tsx
+++ src/components/topic-selector.tsx
@@ -28,7 +28,7 @@ export const TopicSelector: React.FC<TopicSelectorProps> = ({
         <Button
           key={topic.title}
           onClick={() => onTopicSelect(topic)}
-          className="text-lg rounded-full px-4 py-2 bg-muted-yellow hover:bg-yellow-200"
+          className="text-lg rounded-full px-4 py-2 bg-muted-yellow hover:bg-yellow-200 text-black"
         >
           {topic.title}
         </Button>
